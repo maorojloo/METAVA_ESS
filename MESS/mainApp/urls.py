869b9029a4de59769f7c4b5a382863ec1cac7419
MoahmmadAminOrojloo <ma.orojloo@gmail.com>
2023-04-07 16:05:17 +0330

    init

---
+++ MESS/mainApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt import views as jwt_views
+
+
+
+urlpatterns = [
+    path('home/', views.HomeView.as_view(), name ='home'),
+
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+
+    path('logout/', views.LogoutView.as_view(), name ='logout'),
+]
