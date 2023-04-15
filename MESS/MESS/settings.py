bf2a3bf9dae51e0225378e35c9d43fafbf820c34
MohammadAmin Orojloo <orojloomohammadamin@gmail.com>
2023-04-15 15:20:45 +0430

    adding env for pass words

--- MESS/MESS/settings.py
+++ MESS/MESS/settings.py
@@ -1,6 +1,11 @@
 
 from datetime import timedelta
 from pathlib import Path
+import os
+import dotenv # <- New
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,8 +14,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o7226)2nu#yse(vf9a=-t4*ggp&q2!howm#b4(tuu0h4kzuo3_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
